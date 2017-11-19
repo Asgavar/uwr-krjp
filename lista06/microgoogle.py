9e060785767f96bd0e343191cf41f7f2df041c7f
Artur Juraszek <asgavar@gmail.com>
2017-11-19 19:37:33 +0100

    Naprawienie owego zrownoleglenia

--- lista06/microgoogle.py
+++ lista06/microgoogle.py
@@ -6,6 +6,7 @@ import re
 import threading
 import urllib.request
 import html.parser
+import multiprocessing as mp
 
 
 class MicroGoogleHTMLParser(html.parser.HTMLParser):
@@ -38,7 +39,6 @@ class MicroGoogle:
         self.depth = depth
         self.links = []
         self.links.append(startpage)
-        # with urllib.request.urlopen(startpage) as page:
         page = urllib.request.urlopen(startpage)
         # 'superwatek', nie zakonczy sie poki nie zakoncza sie watki wywolane
         # przez niego
@@ -53,11 +53,10 @@ class MicroGoogle:
         Zwraca liste krotek skladajacych sie z adresu strony i liczby wystapien
         szukanego slowa.
         """
-        ret = []
-        for link in self.links:
-            count = self._search(key, link)
-            if count:
-                ret.append((link, count))
+        self._set_keyword(key)
+        with mp.Pool() as pool:
+            ret = pool.map(self._search, self.links)
+        ret = list(filter(lambda x: x[1] != 0, ret))
         ret = sorted(ret, key=lambda x: x[1], reverse=True)
         return ret
 
@@ -67,24 +66,31 @@ class MicroGoogle:
         zaglebiajac sie przy tym nie glebiej niz self.depth.
         """
         html_parser = MicroGoogleHTMLParser()
-        html_parser.feed(page.read().decode('utf-8'))
+        html_parser.feed(str(page.read()))
         for link in html_parser.links:
             if link not in self.links:
                 self.links.append(link)
-            if rec_depth <= self.depth:
-                # with urllib.request.urlopen(link) as next_page:
-                next_page = urllib.request.urlopen(link)
-                threading.Thread(
-                    target=self._get_links,
-                    args=(next_page, rec_depth + 1)
-                )
+                if rec_depth < self.depth:
+                    next_page = urllib.request.urlopen(link)
+                    threading.Thread(
+                        target=self._get_links,
+                        args=(next_page, rec_depth + 1)
+                    ).start()
 
-    def _search(self, keyword, url):
+    def _search(self, url):
         """
         Zwraca liczbe wystapien keyword na stronie pod adresem url.
         """
-        with urllib.request.urlopen(url) as page:
-            return(len(re.findall(keyword, str(page.read()))))
+        print(f'Sprawdzanie {url}')
+        try:
+            with urllib.request.urlopen(url, timeout=10) as page:
+                return(url, len(re.findall(self.keyword, str(page.read()))))
+        except Exception as e:
+            print(f'Pomijanie strony: {url} z powodu: {e}')
+            return (url, 0)
+
+    def _set_keyword(self, keyword):
+        self.keyword = keyword
 
 
 if __name__ == '__main__':
